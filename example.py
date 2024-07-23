f07b8a61639095d13049014fe9609fb16f204688
Hamdi Taha <hamdi.taha@esprit.tn>
2024-07-23 23:50:55 +0200

    metrics eval

--- example.py
+++ example.py
@@ -10,3 +10,10 @@ from sklearn.linear_model import ElasticNet
 
 import mlflow
 import mlflow.sklearn
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
